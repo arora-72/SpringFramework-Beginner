8dfa35f8c7acf63c3f69d5dd98562feba837809c
arora-72 <indresh.arora2016@vitstudent.ac.in>
2019-12-16 20:45:28 +0530

    field injection: Annotation@Autowired

--- sprring-demo-annotations/src/com/ia/springdemo/TennisCoach.java
+++ sprring-demo-annotations/src/com/ia/springdemo/TennisCoach.java
@@ -7,17 +7,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach  implements Coach{
 	
+	@Autowired
 	private FortuneService fortuneService;
 	
 	public TennisCoach() {
 		System.out.println(">> Tenniscoach: inside default constructor");
 	}
 	
+	/*
 	@Autowired
-	public void setFortuneService(FortuneService theFortuneService) {
-		System.out.println(">> TennisCoach: inside setFortuneService");
+	public void doSomeCrazyStuff(FortuneService theFortuneService) {
+		System.out.println(">> TennisCoach: inside doSomeCrazyStuff");
 		fortuneService = theFortuneService;
 	}
+	*/
 	
 	/*
 	//constructor injection
